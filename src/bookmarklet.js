963343f4d6f9e51e011d79b498f49d362f66ab98
Wizard <rcvbuf@gmail.com>
2024-10-16 01:34:47 +0800

    send 2 msgs!

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -2,53 +2,56 @@ javascript: (function () {
   function writeToInputBox(text) {
     const inputBox = document.querySelector('div[contenteditable="true"][role="textbox"]');
     if (inputBox) {
-      inputBox.focus();
-      const clickEvent = new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-        view: window
-      });
-      inputBox.dispatchEvent(clickEvent);
-      inputBox.innerHTML = '';
-      let i = 0;
-      function typeNextChar() {
-        if (i < text.length) {
-          inputBox.innerHTML += text.charAt(i);
-          i++;
-          const inputEvent = new Event('input', { bubbles: true, cancelable: true });
-          inputBox.dispatchEvent(inputEvent);
-          setTimeout(typeNextChar, 10);
-        } else {
-          const keyEvent = new KeyboardEvent('keydown', {
-            bubbles: true,
-            cancelable: true,
-            key: 'Enter',
-            keyCode: 13
-          });
-          inputBox.dispatchEvent(keyEvent);
-        }
+      const targetDiv = inputBox.parentElement;
+      if (targetDiv) {
+        console.log("点击目标 div");
+        targetDiv.click();
+        console.log("已点击目标 div");
       }
-      setTimeout(typeNextChar, 100);
+
+      setTimeout(() => {
+        inputBox.focus();
+        inputBox.textContent = '';
+        console.log("开始输入 " + text);
+
+        function simulateTyping(text, index = 0) {
+          if (index < text.length) {
+            document.execCommand('insertText', false, text[index]);
+            setTimeout(() => simulateTyping(text, index + 1), 10);
+          } else {
+            console.log("输入完成");
+            const enterEvent = new KeyboardEvent('keydown', {
+              bubbles: true,
+              cancelable: true,
+              key: 'Enter',
+              keyCode: 13
+            });
+            inputBox.dispatchEvent(enterEvent);
+            sendMessage();
+          }
+        }
+
+        simulateTyping(text);
+      }, 100);
     } else {
       console.log("未找到输入框");
     }
   }
   function sendMessage() {
-    setTimeout(() => {
-      const sendButton = document.querySelector('div[aria-label="Press Enter to send"]');
-      if (sendButton) {
-        sendButton.click();
-      } else {
-        console.log("未找到发送按钮");
-      }
-    }, 100);
+    const sendButton = document.querySelector('div[aria-label="Press Enter to send"]');
+    if (sendButton) {
+      sendButton.click();
+      console.log("消息已发送");
+    } else {
+      console.log("未找到发送按钮");
+    }
   }
   const messages = document.getElementsByClassName("x78zum5 xdt5ytf x1iyjqo2 x2lah0s xl56j7k x121v3j4")[0];
   messages.removeEventListener("DOMNodeInserted", null);
-  messages.addEventListener("DOMNodeInserted", async (event) => {
-    console.log("domnodeinserted event target is:", event?.target);
+  messages.addEventListener("DOMNodeInserted", async (event) => {    
     const imgSrc = event?.target?.getElementsByTagName("img")[1]?.src;
     if (imgSrc) {
+      console.log("send image to server");
       const res = await fetch("http://localhost:3103/api/gpt-4-vision", {
         method: "POST",
         body: JSON.stringify({ imageUrl: imgSrc }),
@@ -62,10 +65,6 @@ javascript: (function () {
         writeToInputBox(data);
         setTimeout(resolve, 1000);
       });
-      sendMessage();
-    }
-    else {
-      console.log("未找到图片1");
     }
   });
   alert("已添加 Messenger 聊天观察器");
